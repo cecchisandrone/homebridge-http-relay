1d60e04b8475f3a6561202adda154ef8c67a34b0
Alessandro Dionisi <cecchisandrone@gmail.com>
2017-07-29 14:39:43 +0200

    First version

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var gpio = require('pi-gpio');
+var request = require('request');
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-
-    homebridge.registerAccessory('homebridge-gpio', 'GPIO', GPIOAccessory);
+    homebridge.registerAccessory('homebridge-http-relay', 'HttpRelay', HttpRelayAccessory);
 }
 
-function GPIOAccessory(log, config) {
+function HttpRelayAccessory(log, config) {
     this.log = log;
     this.name = config['name'];
-    this.pin = config['pin'];
     this.duration = config['duration'];
     this.service = new Service.Switch(this.name);
+    this.value = false;
 
-    if (!this.pin) throw new Error('You must provide a config value for pin.');
+    if (!this.duration) 
+        throw new Error('You must provide a config value for duration.');
 
     this.service
         .getCharacteristic(Characteristic.On)
@@ -24,51 +25,26 @@ function GPIOAccessory(log, config) {
 
 }
 
-GPIOAccessory.prototype.getServices = function() {
+HttpRelayAccessory.prototype.getServices = function() {
     return [this.service];
 }
 
-GPIOAccessory.prototype.getOn = function(callback) {
-    gpio.read(this.pin, function(err, value) {
-        if (err) {
-        	callback(err);
-        } else {
-	        var on = value;
-    	    callback(null, on);
-    	}
+HttpRelayAccessory.prototype.getOn = function(callback) {
+    this.log("Getting relay status");
+    callback(null, this.value);
+}
+
+HttpRelayAccessory.prototype.setOn = function(on, callback) {
+    this.log("Toggling relay");
+    this.value = true;
+    var that = this;
+    request.post('http://localhost:5000/toggle-relay?duration=' + this.duration, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+        that.value = false;
+        callback(null, that.value);
     });
-}
-
-GPIOAccessory.prototype.setOn = function(on, callback) {
-    if (on) {
-        this.pinAction(0);
-		if (is_defined(this.duration) && is_int(this.duration)) {
-			this.pinTimer()
-		}
-		callback(null);
-    } else {
-		this.pinAction(1);
-		callback(null);
-    }
-}
-
-GPIOAccessory.prototype.pinAction = function(action) {
-        this.log('Turning ' + (action == 0 ? 'on' : 'off') + ' pin #' + this.pin);
-
-        var self = this;
-        gpio.open(self.pin, 'output', function() {
-        gpio.write(self.pin, action, function() {
-		gpio.close(self.pin);
-		return true;
-        });
-    });
-}
-
-GPIOAccessory.prototype.pinTimer = function() {
-        var self = this;
-        setTimeout(function() {
-			self.pinAction(1);
-        }, this.duration);
 }
 
 var is_int = function(n) {
